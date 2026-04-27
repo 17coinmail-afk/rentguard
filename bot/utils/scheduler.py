6a965e9b268b9cfb9ddca17d4074f3e66ead08e3
RentGuard Deploy <deploy@rentguard.local>
2026-04-27 19:09:09 +0800

    chore: clear project for new idea

--- bot/utils/scheduler.py
+++
@@ -1,9 +0,0 @@
-# APScheduler removed due to Render compatibility issues
-# Notifications will be added back in future update
-
-class FakeScheduler:
-    def start(self):
-        pass
-
-def setup_scheduler(bot):
-    pass
