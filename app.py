dd23161937610ec8fc471778d75e25623b850cc7
RentGuard Deploy <deploy@rentguard.local>
2026-04-27 12:52:06 +0800

    test: aiohttp + aiogram import

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from aiohttp import web
 import os
 
+# Test if aiogram import works
+from aiogram import Bot
+API_TOKEN = os.getenv("BOT_TOKEN", "")
+bot = Bot(token=API_TOKEN)
+
 async def hello(request):
-    return web.Response(text="Hello, world")
+    return web.Response(text="Hello, world with aiogram")
 
 app = web.Application()
 app.router.add_get("/", hello)
